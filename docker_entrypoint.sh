9ea666c14892dc6eff79adcc03788855f90333d5
Bryan Konowitz <bryan@kono.sh>
2018-04-05 23:01:20 -0700

    WIP

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
-rm /out/app.zip >/dev/null 2>&1
+echo "starting build..."
+# rm /out/app.zip >/dev/null 2>&1
+echo "... skipping rm"
+echo "... attempting copy..."
 cp /app/app.zip /out/
+echo "... copy complete. all done!"
