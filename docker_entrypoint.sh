1b467afc0c7703ba146d83794dec92f81dea8668
Bryan Konowitz <bryan@kono.sh>
2018-04-05 22:52:47 -0700

    Updated buildspec.yml to support CodePipeline usage of CodeBuild

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
+echo "starting build..."
+echo "... rm the old stuff"
 rm /out/app.zip >/dev/null 2>&1
+echo "... attempting copy"
 cp /app/app.zip /out/
+echo "... copy complete."
+ls -al /out/
+echo "... all done!"
